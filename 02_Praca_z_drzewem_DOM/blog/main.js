587d8f800e0a6ab35210628e49b3f95d915c930f
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-03-15 20:34:15 +0100

    work with dom model

---
+++ 02_Praca_z_drzewem_DOM/blog/main.js
@@ -0,0 +1,8 @@
+//Zadanie 1
+
+//1. Nagłówek
+const header = document.querySelector(".about-blog__content-title");
+console.log(header);
+
+//2. Logo
+const logo = document.querySelector(".navigation-menu img");
