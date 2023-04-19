289ad4f7322c959871356f15321a7e6ca3887007
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-04-19 20:55:15 +0200

    arrow functions all

--- 04_ES6/01_arrowFunctions/Zadania/main.js
+++ 04_ES6/01_arrowFunctions/Zadania/main.js
@@ -27,3 +27,38 @@ const runInterval = (n = 8) => {
 }
 
 runInterval();
+
+
+//Zadanie 3
+
+const generateLinks = (tab) => {
+    return tab.map(el => `https://${el}.com`);
+}
+
+console.log(generateLinks(["google", "twitter", "facebook"]));
+
+
+const insertLinks = (tab, query) => {
+    tab.forEach((el) => {
+        console.log(el);
+        query.innerHTML += `<li><a href="${el}">${el}</a></li>`;
+    })
+}
+
+const insertLinks2 = (tab, query) => {
+    tab.forEach((el) => {
+        const newLi = document.createElement("li");
+        const newA = document.createElement("a");
+        newA.setAttribute("href", el);
+        newA.textContent = el;
+        newLi.append(newA);
+        query.appendChild(newLi);
+    })
+}
+
+const tab = generateLinks(["google", "twitter", "facebook"]);
+const menu = document.querySelector("#menu");
+const menu2 = document.querySelector("#menu2");
+
+insertLinks(tab, menu);
+insertLinks2(tab, menu2);
