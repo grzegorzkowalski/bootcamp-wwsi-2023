3d9a772525a7bdc1723d4af8556e2782992ab39f
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-04-19 20:15:52 +0200

    Chapter 4 Sub 01 Task 2

--- 04_ES6/01_arrowFunctions/Zadania/main.js
+++ 04_ES6/01_arrowFunctions/Zadania/main.js
@@ -1,3 +1,5 @@
+//Zadanie 1
+
 const getSecondMaxNumber = (tab) => {
     const sortedTab = [...tab];
     sortedTab.sort((a,b) => a-b);
@@ -7,3 +9,21 @@ const getSecondMaxNumber = (tab) => {
 const arr1 = [2, 3, 1, 6, 100, 49, 5, 7, 8, 9 ];
 console.log(getSecondMaxNumber(arr1));
 console.log(arr1);
+
+
+//Zadanie 2
+
+const runInterval = (n = 8) => {
+    let i = 0;
+    const intervalID = setInterval(() => {
+        if (i < n) {
+            console.log("Hello");
+            i++;
+        }
+        else {
+            clearInterval(intervalID);
+        }
+    }, 500);
+}
+
+runInterval();
